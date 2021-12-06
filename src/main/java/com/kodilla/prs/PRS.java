c77863e81c462d8167559208578e96204503205e
Filip Gabrys <el.filberto@gmail.com>
2021-12-06 16:18:18 +0100

    Fix the WrongKeyException

--- src/main/java/com/kodilla/prs/PRS.java
+++ src/main/java/com/kodilla/prs/PRS.java
@@ -19,75 +19,91 @@ public class PRS {
                 "3 - choose Scissors");
         try {
             whatToDo();
-        } catch ( WrongKeyException e) {
-            System.out.println("Wrong Key, try again");
+        } catch (WrongKeyException e) {
+            System.out.println("Wrong value, please try again");
+            whatToDo();
         }
     }
 
     private static void whatToDo() throws WrongKeyException {
         System.out.println("What would you like to do?");
         String choice = (new Scanner(System.in)).next();
-        if (choice.equals("n")) {
-            newGameCommand();
-        } else if (choice.equals("x")) {
-            exitGameCommand();
+        if (choice.equals("n") || choice.equals("x")) {
+            if (choice.equals("n")) {
+                newGameCommand();
+            } else if (choice.equals("x")) {
+                exitGameCommand();
+            }
+        } else {
+            throw new WrongKeyException();
         }
-        else whatToDo();
     }
 
     private static void exitGameCommand() throws WrongKeyException {
         System.out.println("Are you sure you want to leave the game? y/n");
         String choice = (new Scanner(System.in)).next();
-        if (choice.equals("y")) {
-            System.exit(0);
+        if (choice.equals("y") || choice.equals("n")) {
+            if (choice.equals("y")) {
+                System.exit(0);
+            } else {
+                whatToDo();
+            }
         } else {
-            whatToDo();
+            throw new WrongKeyException();
         }
     }
 
     private static void newGameCommand() throws WrongKeyException {
         System.out.println("Are you sure you want to start a new game? y/n");
         String choice = (new Scanner(System.in)).next();
-        if (choice.equals("y")) {
-            startAGame();
+        if (choice.equals("y") || choice.equals("n")) {
+            if (choice.equals("y")) {
+                startAGame();
+            } else {
+                whatToDo();
+            }
         } else {
-            whatToDo();
+            throw new WrongKeyException();
         }
     }
 
     private static void startAGame() throws WrongKeyException {
         System.out.println("Please type in how many wins shall determine the winner of this battle: ");
         Scanner rounds = new Scanner(System.in);
-        int r = rounds.nextInt();
-        System.out.println("Well then, let the battle begin till someone wins " + r + " rounds!");
-        int comWin = 0;
-        int plaWin = 0;
+        if (rounds.hasNextInt()) {
+            int r = rounds.nextInt();
+            System.out.println("Well then, let the battle begin till someone wins " + r + " rounds!");
+            int comWin = 0;
+            int plaWin = 0;
 
-        while (comWin != r || plaWin != r) {
-            System.out.println("Choose your shape");
-            Scanner pick = new Scanner(System.in);
-            Hand plaShape = getPlayerShape(pick);
-            Hand comShape = getComputerShape();
-            System.out.println(plaShape + "  :  " + comShape);
-            if (plaShape.getShape().equals(comShape.getShape())) {
-                System.out.println("It's a tie!");
-                System.out.println(comWin + "  :  " + plaWin);
-            } else if (plaShape.getShape().equals(HandShape.ROCK)) {
-                System.out.println(comShape.getShape().equals(HandShape.PAPER) ? "Computer Scores!" : "Player Scores!");
-                System.out.println(comShape.getShape().equals(HandShape.PAPER) ? comWin++ + " : " + plaWin : comWin + " : " + plaWin++);
-            } else if (plaShape.getShape().equals(HandShape.PAPER)) {
-                System.out.println(comShape.getShape().equals(HandShape.SCISSORS) ? "Computer Scores!" : "Player Scores!");
-                System.out.println(comShape.getShape().equals(HandShape.SCISSORS) ? comWin++ + " : " + plaWin : comWin + " : " + plaWin++);
-            } else if (plaShape.getShape().equals(HandShape.EMPTY)) {
-                System.out.println("Try again");
-            } else {
-                System.out.println(comShape.getShape().equals(HandShape.ROCK) ? "Computer Scores!" : "Player Scores!");
-                System.out.println(comShape.getShape().equals(HandShape.ROCK) ? comWin++ + " : " + plaWin : comWin + " : " + plaWin++);
+            while (comWin != r || plaWin != r) {
+                System.out.println("Choose your shape");
+                Scanner pick = new Scanner(System.in);
+                Hand plaShape = getPlayerShape(pick);
+                Hand comShape = getComputerShape();
+                System.out.println(plaShape + "  :  " + comShape);
+                if (plaShape.getShape().equals(comShape.getShape())) {
+                    System.out.println("It's a tie!");
+                    System.out.println(comWin + "  :  " + plaWin);
+                } else if (plaShape.getShape().equals(HandShape.ROCK)) {
+                    System.out.println(comShape.getShape().equals(HandShape.PAPER) ? "Computer Scores!" : "Player Scores!");
+                    System.out.println(comShape.getShape().equals(HandShape.PAPER) ? comWin++ + " : " + plaWin : comWin + " : " + plaWin++);
+                } else if (plaShape.getShape().equals(HandShape.PAPER)) {
+                    System.out.println(comShape.getShape().equals(HandShape.SCISSORS) ? "Computer Scores!" : "Player Scores!");
+                    System.out.println(comShape.getShape().equals(HandShape.SCISSORS) ? comWin++ + " : " + plaWin : comWin + " : " + plaWin++);
+                } else if (plaShape.getShape().equals(HandShape.EMPTY)) {
+                    System.out.println("Try again");
+                } else {
+                    System.out.println(comShape.getShape().equals(HandShape.ROCK) ? "Computer Scores!" : "Player Scores!");
+                    System.out.println(comShape.getShape().equals(HandShape.ROCK) ? comWin++ + " : " + plaWin : comWin + " : " + plaWin++);
+                }
+                System.out.println(" ");
             }
-            System.out.println(" ");
+            System.out.println(plaWin == r ? "Player wins the game!" : "Computer wins the game!");
+            whatToDo();
+        } else {
+            throw new WrongKeyException();
         }
-        System.out.println(plaWin == r ? "Player wins the game!" : "Computer wins the game!");
-        whatToDo();
     }
 
     private static Hand getComputerShape() {
